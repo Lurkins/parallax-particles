b0bf74917b182c38765640ebc0179d13a5981033
Paul Perkins <paulperkins7@gmail.com>
2019-09-03 22:59:52 -0500

    added project files

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    Particles.init({
+        selector: '.background',
+        maxParticles: 170,
+        sizeVariations: 3,
+        speed: .2,
+        color: '#2882ff',
+        minDistrance: 120,
+        connectParticles: true
+    });
+};
